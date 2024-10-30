eb52207b27de2d17a86ab78644fce9f67118fd7c
SandraAdel66 <110496848+SandraAdel66@users.noreply.github.com>
2024-10-30 20:38:21 +0300

    Add files via upload

---
+++ Inpainting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Inpainting.css';
+
+function Inpainting() {
+  return (
+    <div className="inpainting-container">
+      <h1>Inpainting</h1>
+      <p>Content related to Inpainting will be available soon...</p>
+    </div>
+  );
+}
+
+export default Inpainting;
---
+++ SuperResolution.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SuperResolution.css';
+
+function SuperResolution() {
+  return (
+    <div className="super-resolution-container">
+      <h1>Super Resolution</h1>
+      <p>Content related to Super Resolution will be available soon...</p>
+    </div>
+  );
+}
+
+export default SuperResolution;
